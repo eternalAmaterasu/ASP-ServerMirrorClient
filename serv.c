f32084a2450c8882f9c814156969d9c793c05bdb
Vivek <verma17@uwindsor.ca>
2023-04-10 20:41:37 -0400

    logic propagation in server - to handle the mirror registration and the client bifurcation and required functions

--- serv.c
+++ serv.c
@@ -8,65 +8,106 @@
 #include <sys/socket.h>
 #include <unistd.h>
 
-int main(int argc, char const *argv[]) {
+#define SERVER_PORT 8080
+#define MIRROR_PORT 8081
 
-    int server_fd, new_socket;
+int SOCKET_WAITING_QUEUE_SIZE = 3;
+int IP_LENGTH = 25;
+char *mirrorIp = NULL;
+char *redirectMessageForClient = NULL;
+char *mirrorRegistrationStartingMessage = "Mir=";
+char *MIRROR_ACK = "OK";
+
+int decideConnectionServer(int connectionNumber) {
+    return connectionNumber <= 4 || (connectionNumber >= 9 && (connectionNumber & 1) == 1);
+}
+
+void processClient(int socket, char buffer[], long int bytesRead) {
+    int cid = fork();
+    if (cid == 0) {
+        printf("%d => %ld bytes read. String => '%s'\n", getpid(), bytesRead, buffer);
+        send(socket, MIRROR_ACK, strlen(MIRROR_ACK), 0);
+        printf("Hello message sent\n");
+
+        // closing the connected socket
+        close(socket);
+        exit(0);
+    } else {
+        printf("from the root process.. nothing else to do?\n");
+    }
+}
+
+int main(int argc, char const *argv[]) {
+    int root = getpid();
+    printf("root pid: %d\n", root);
+
+    mirrorIp = malloc(IP_LENGTH * sizeof(char));
+    redirectMessageForClient = malloc((IP_LENGTH + 6) * sizeof(char));
+
+    int server_fd, socketConn;
     long int bytesRead;
     struct sockaddr_in address;
     int opt = 1;
     int addrlen = sizeof(address);
-    char buffer[1024] = {0};
-    char *hello = "OK";
 
-    // Creating socket file descriptor
     if ((server_fd = socket(AF_INET, SOCK_STREAM, 0)) < 0) { //AF_INET for IPv4, SOCK_STREAM -- TCP
         perror("socket failed");
         exit(EXIT_FAILURE);
     }
 
-    // Forcefully attaching socket to the port 8080
     if (setsockopt(server_fd, SOL_SOCKET, SO_REUSEADDR | SO_REUSEPORT, &opt, sizeof(opt))) {
         perror("setsockopt");
         exit(EXIT_FAILURE);
     }
     address.sin_family = AF_INET;
     address.sin_addr.s_addr = INADDR_ANY;
-    address.sin_port = htons(8080);
+    address.sin_port = htons(SERVER_PORT);
 
-    // Forcefully attaching socket to the port 8080
     if (bind(server_fd, (struct sockaddr *) &address, sizeof(address)) < 0) {
         perror("bind failed");
         exit(EXIT_FAILURE);
     }
-    if (listen(server_fd, 3) < 0) { //limiting to 3 requests in the queue
+    if (listen(server_fd, SOCKET_WAITING_QUEUE_SIZE) < 0) { //limiting to 3 requests in the queue
         perror("listen");
         exit(EXIT_FAILURE);
     }
-    printf("Listen up... \n");
-    if ((new_socket = accept(server_fd, (struct sockaddr *) &address, (socklen_t *) &addrlen)) < 0) {
-        perror("accept");
-        exit(EXIT_FAILURE);
-    }
-    printf("Waiting for accepting..\n");
-    int root = getpid();
-    printf("root pid: %d\n", root);
-    int cid = fork();
-    if (cid == 0) {
-        bytesRead = read(new_socket, buffer, 1024);
-        printf("%d => %ld bytes read. String => '%s'\n", getpid(), bytesRead, buffer);
-        send(new_socket, hello, strlen(hello), 0);
-        printf("Hello message sent\n");
 
-        // closing the connected socket
-        close(new_socket);
-        exit(0);
-    } else {
-        close(new_socket);
-        printf("from the root process.. nothing else to do?\n");
-        sleep(10);
-        printf("sleep over..\n");
+    int connectionsServiced = 0;
+    printf("Entering listening state now...\n");
+    while (1) {
+        printf("Waiting for accepting..\n");
+        if ((socketConn = accept(server_fd, (struct sockaddr *) &address, (socklen_t *) &addrlen)) < 0) {
+            perror("accept");
+            exit(EXIT_FAILURE);
+        }
+        char buffer[1024] = {0};
+
+        bytesRead = read(socketConn, buffer, 1024);
+        if (bytesRead == 18 && strncmp(mirrorRegistrationStartingMessage, buffer, 4) == 0) {
+            strncpy(redirectMessageForClient, buffer, 18);
+            //this msg was sent by the mirror, and shall not count as a client connection.
+            for (int i = 0; i < IP_LENGTH && buffer[i + 4] != ';'; i++)
+                mirrorIp[i] = buffer[i + 4];
+
+            send(socketConn, MIRROR_ACK, strlen(MIRROR_ACK), 0);
+            printf("Registered mirror ip as: %s, of length: %lu\n", mirrorIp, strlen(mirrorIp));
+            continue;
+        }
+
+        connectionsServiced++;
+        if (decideConnectionServer(connectionsServiced)) {
+            //server will handle this request
+            printf("Accepted a request from the queue..\n");
+            processClient(socketConn, buffer, bytesRead);
+
+        } else {
+            //mirror will handle this request
+            printf("Advising redirect to the client with the mirror address. Server will not service this request..\n");
+            send(socketConn, redirectMessageForClient, strlen(redirectMessageForClient), 0);
+            printf("Redirect command sent to client..\n");
+        }
     }
-    // closing the listening socket
+    // closing the listening socketConn
     shutdown(server_fd, SHUT_RDWR);
     return 0;
 }
