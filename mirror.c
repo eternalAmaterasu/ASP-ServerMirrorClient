adef598955bbfd31b09b286a318adb60ae262937
Vivek <verma17@uwindsor.ca>
2023-04-14 01:06:28 -0400

    Incorporating server main processing logic into mirror

--- mirror.c
+++ mirror.c
@@ -10,15 +10,23 @@
 #include <malloc.h>
 #include <stdlib.h>
 #include <signal.h>
+#include <sys/stat.h>
+#include <ctype.h>
+#include <fcntl.h>
 
 #define SERVER_PORT 8080
 #define MIRROR_PORT 8081
 #define NULL_CH '\0'
 
 int SOCKET_WAITING_QUEUE_SIZE = 3;
+int IP_LENGTH = 25;
 int BUFFER_LENGTH = 1024;
+int MAX_WORDS = 128;
+int MAX_WORD_LENGTH = 256;
+
 char *MIRROR_ACK = "OK";
-char *serverIpInstance;
+char *serverIpInstance = NULL;
+char *CLIENT_ACK = "Hi";
 
 char *getCurrentHostIp() {
     int IP_LENGTH = 25;
@@ -61,8 +69,12 @@ int connectAndGetFd(char *serverIp, int port) {
     return fdClient;
 }
 
+void cleanBufferWithLength(char data[], int length) {
+    for (int i = 0; i < length; i++) data[i] = NULL_CH;
+}
+
 void cleanBuffer(char data[]) {
-    for (int i = 0; i < BUFFER_LENGTH; i++) data[i] = NULL_CH;
+    cleanBufferWithLength(data, BUFFER_LENGTH);
 }
 
 int registerOntoServer(char *hostIp, char *serverIp) {
@@ -107,17 +119,220 @@ void deregisterFromServer() {
     close(fd_mirror_client);
 }
 
-void processClient(int socket, char buffer[], long int bytesRead) {
-    if (!fork()) {
-        printf("%d => %ld bytes read. String => '%s'\n", getpid(), bytesRead, buffer);
-        char *clientMessage = "Monetary case from mirror";
-        send(socket, clientMessage, strlen(clientMessage), 0);
-        printf("Hello message sent\n");
+void send_file(int sockfd) {
+    char buffer[BUFFER_LENGTH];
 
-        // closing the connected socket
-        close(socket);
-        exit(0);
+    FILE *file = fopen("temp.tar.gz", "rb");
+    if (file == NULL) {
+        perror("Error opening file");
+        exit(EXIT_FAILURE);
     }
+
+    // Get the file size
+    struct stat file_stat;
+    if (fstat(fileno(file), &file_stat) < 0) {
+        perror("Error getting file size");
+        exit(EXIT_FAILURE);
+    }
+
+    // Send the file size to the client
+    size_t file_size = file_stat.st_size;
+    send(sockfd, &file_size, sizeof(file_size), 0);
+
+    // Send the file
+    while (!feof(file)) {
+        size_t bytes_read = fread(buffer, 1, BUFFER_LENGTH, file);
+        send(sockfd, buffer, bytes_read, 0);
+    }
+
+    fclose(file);
+}
+
+
+int split_words(char input_string[], char words[MAX_WORDS][MAX_WORD_LENGTH]) {
+    int word_count = 0;
+    char *ptr = input_string;
+    while (*ptr != '\0') {
+        while (*ptr == ' ') ptr++;
+        if (*ptr == '\0') break;
+        char *word_start = ptr;
+        while (*ptr != ' ' && *ptr != '\0') ptr++;
+        strncpy(words[word_count], word_start, ptr - word_start);
+        words[word_count][ptr - word_start] = '\0';
+        word_count++;
+        if (*ptr == ' ') ptr++;
+    }
+    return word_count;
+}
+
+void trim(char *str) {
+    char *start, *end;
+    for (start = str; *start && isspace(*start); ++start);
+    if (start != str) {
+        memmove(str, start, strlen(start) + 1);
+    }
+    for (end = str + strlen(str) - 1; end > str && isspace(*end); --end);
+    *(end + 1) = '\0';
+}
+
+void process_command(int socket, char input_string[]) {
+    char output_message[2056];
+    trim(input_string);
+    char words[MAX_WORDS][MAX_WORD_LENGTH];
+    int word_count = split_words(input_string, words);
+    if (strcmp(words[0], "findfile") == 0) {
+        char command[1024] = {0};
+        //printf("\nSearch Filename %s\n",words[1]);
+        system("rm -f output.txt");
+        snprintf(command, sizeof(command), "find ~ -type f -name \"%s\" -printf \"File: %%p\\nSize: %%s bytes\\nCreation date: %%Tc\\n\" -quit > output.txt", words[1]);
+        FILE *pipe = popen(command, "r");
+        if (pipe == NULL) {
+            printf("\nError: Could Not Execute find Command\n");
+            char error_message[] = "Some error occurred while processing the request";
+            strcpy(output_message, error_message);
+            cleanBuffer(output_message);
+            send(socket, output_message, strlen(output_message), 0);
+        }
+        pclose(pipe);
+        int fd = open("output.txt", O_RDONLY);
+        char buffer[2056] = {0};
+        int bytes_read = read(fd, buffer, 2056);
+        close(fd);
+        //system("rm -f output.txt");
+        cleanBuffer(output_message);
+        if (bytes_read == 0)
+            strcpy(output_message, "File not found");
+        else
+            strcpy(output_message, buffer);
+        send(socket, output_message, strlen(output_message), 0);
+    } else if (strcmp(words[0], "sgetfiles") == 0) {
+        char command[1024] = {0};
+        printf("\nSize1 %s Size2 %s\n", words[1], words[2]);
+        int size1 = atoi(words[1]);
+        int size2 = atoi(words[2]);
+        system("rm -f temp.tar.gz");
+        snprintf(command, sizeof(command), "find ~ -type f -size +%dc -a -size -%dc -print0 | tar -czvf temp.tar.gz --null -T -", size1, size2);
+        FILE *pipe = popen(command, "r");
+        if (pipe == NULL) {
+            printf("\nError: Could Not Execute find Command\n");
+            char error_message[] = "Some error occurred while processing the request";
+            cleanBuffer(output_message);
+            strcpy(output_message, error_message);
+        }
+        pclose(pipe);
+        send_file(socket);
+        //system("rm -f temp.tar.gz");
+    } else if (strcmp(words[0], "dgetfiles") == 0) {
+        char command[1024] = {0};
+        printf("\nDate1 %s Date2 %s\n", words[1], words[2]);
+        system("rm -f temp.tar.gz");
+        snprintf(command, sizeof(command), "find ~ -type f -newermt \"%s\" ! -newermt \"%s\" -print0 | tar -czvf temp.tar.gz --null -T -", words[1], words[2]);
+        FILE *pipe = popen(command, "r");
+        if (pipe == NULL) {
+            printf("\nError: Could Not Execute find Command\n");
+            char error_message[] = "Some error occurred while processing the request";
+            strcpy(output_message, error_message);
+        }
+        pclose(pipe);
+        send_file(socket);
+        //system("rm -f temp.tar.gz");
+    } else if (strcmp(words[0], "getfiles") == 0) {
+        system("rm -f output.txt");
+        system("chmod +x ./find_files_by_name.sh");
+        char command[1024] = {0};
+        strcat(command, "./find_files_by_name.sh ");
+        for (int i = 1; i < word_count; i++) {
+            if (strcmp(words[i], "-u") != 0) {
+                strcat(command, words[i]);
+                strcat(command, " ");
+            }
+        }
+        strcat(command, " > output.txt");
+        printf("\nCommand: %s\n", command);
+        system("rm -f temp.tar.gz");
+        FILE *pipe = popen(command, "r");
+        if (pipe == NULL) {
+            printf("\nError: Could Not Execute find Command\n");
+            char error_message[] = "Some error occurred while processing the request";
+            strcpy(output_message, error_message);
+        }
+        pclose(pipe);
+        int fd = open("output.txt", O_RDONLY);
+        char buffer[2056] = {0};
+        int bytes_read = read(fd, buffer, 2056);
+        close(fd);
+        cleanBuffer(output_message);
+        trim(buffer);
+        if (strcmp(buffer, "temp.tar.gz created with the found files.") == 0) {
+            strcpy(output_message, "File found");
+            send(socket, output_message, strlen(output_message), 0);
+            send_file(socket);
+        } else {
+            strcpy(output_message, "No file found");
+            send(socket, output_message, strlen(output_message), 0);
+        }
+        //system("rm -f temp.tar.gz");
+        //system("rm -f output.txt");
+    } else if (strcmp(words[0], "gettargz") == 0) {
+        system("rm -f output.txt");
+        system("chmod +x ./find_files_by_extension.sh");
+        char command[1024] = {0};
+        strcat(command, "./find_files_by_extension.sh ");
+        for (int i = 1; i < word_count; i++) {
+            if (strcmp(words[i], "-u") != 0) {
+                strcat(command, words[i]);
+                strcat(command, " ");
+            }
+        }
+        printf("\nCommand: %s\n", command);
+        system("rm -f temp.tar.gz");
+        FILE *pipe = popen(command, "r");
+        if (pipe == NULL) {
+            printf("\nError: Could Not Execute find Command\n");
+            char error_message[] = "Some error occurred while processing the request";
+            strcpy(output_message, error_message);
+        }
+        pclose(pipe);
+        struct stat buffer;
+        int file_exists = stat("./temp.tar.gz", &buffer) == 0;
+        cleanBuffer(output_message);
+        if (file_exists) {
+            strcpy(output_message, "File found");
+            trim(output_message);
+            send(socket, output_message, strlen(output_message), 0);
+            send_file(socket);
+        } else {
+            strcpy(output_message, "No file found");
+            send(socket, output_message, strlen(output_message), 0);
+        }
+        //system("rm -f temp.tar.gz");
+        //system("rm -f output.txt");
+    }
+}
+
+void processClient(int socket, char buffer[], long int bytesRead) {
+    int pid = fork();
+    if (pid == 0) {
+        printf("%d => %ld bytes read. String => '%s'\n", getpid(), bytesRead, buffer);
+        send(socket, CLIENT_ACK, strlen(CLIENT_ACK), 0);
+        while (1) {
+            char input_string[2056] = {0};
+            long int bytes_received = read(socket, input_string, 2056);
+            trim(input_string);
+            if (strcmp(input_string, "quit") == 0) {
+                close(socket); // alert - added here
+                printf("\nSERVER(%d): Shutting down. Closing connection. \n", getpid());
+                exit(0);
+            }
+            process_command(socket, input_string);
+        }
+    } else if (pid < 0) {
+        perror("fork failed");
+        exit(EXIT_FAILURE);
+    }
+
+    // closing the connected socket
+    close(socket);
 }
 
 void handleInterrupt() {
